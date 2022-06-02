80385a8ed91a6c34bc88fa9989dd83bbcd06e961
tsehayget <tsehayget4@gmail.com>
2022-06-02 15:13:33 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,7 @@
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n",
-			s);
-			return (1);
+	write(2, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n",
+			sizeof("and that piece of art is useful\"-Dora Korpar, 2015-10-19"));
+	return (1);
 }
