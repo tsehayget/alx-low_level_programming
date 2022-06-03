a174c3e74fe32f8f0d8d96beb13ec00d8229ea8b
tsehayget <tsehayget4@gmail.com>
2022-06-03 08:42:26 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int g;
+
+	for (g = 'a'; g <= 'z'; g++)
+	{
+		putchar(g);
+	}
+	putchar('\n');
+	return (0);
+}
