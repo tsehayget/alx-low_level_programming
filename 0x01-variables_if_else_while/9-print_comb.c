14fbd90f360c6b031620196363ac0075c6c9da94
tsehayget <tsehayget4@gmail.com>
2022-06-03 12:28:13 -0700

    print correction

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,14 @@ int main(void)
 {
 	int i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar(i);
-		if (i < 57)
-		{
-			putchar(' ');
+		putchar((i % 10) + '0');
+		if (i == 9)
+		continue;
 			putchar(',');
-		}
+			putchar(' ');
+		
 	}
 	putchar('\n');
 	return (0);
