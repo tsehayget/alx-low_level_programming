305e5a9e90555c1cd07b60a3fa3a31caa0b5b57e
tsehayget <tsehayget4@gmail.com>
2022-06-07 08:58:06 -0700

    character _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
-#include<main.h>
+#include "main.h"
 /**
  * main - program prints _putchar followed by a new line
  * Return: 0
  */
 int main(void)
 {
-	putchar("_putchar\n");
+	char c[] = "_putchar/n";
+	int a;
+
+	for (a = 0; a < 9; a++)
+	{
+		_putchar(c[a]);
+	}
 
 	return (0);
 }
