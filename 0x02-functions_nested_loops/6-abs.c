17f4cbc8b513fb824e7a9d925eadefb37f58179c
tsehayget <tsehayget4@gmail.com>
2022-06-07 14:41:27 -0700

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,11 +3,11 @@
  * _abs - computs absolute value
  *
  * 'int' _abs: int type number
- * n: pulled from the main file
+ * @n: pulled from the main file
  *
  * Return: Returns result.
  */
-int _abs(int)
+int _abs(int n)
 {
 	if (n > 0)
 	{
