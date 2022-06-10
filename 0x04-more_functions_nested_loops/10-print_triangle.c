c73c4326c295e4037d3350788cb4539f81703de8
tsehayget <tsehayget4@gmail.com>
2022-06-09 18:33:39 -0700

    commit m

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangle
+ *
+ * @size: variable input originates from main
+ */
+void print_triangle(int size)
+{
+	int hash, index;
+	if (size > 0)
+	{
+		for (hash = 1; hash <= size; hash++)
+		{
+			for (index = size - hash; index > 0; index--)
+				_putchar(' ');
+			for (index = 0; index < hash; index++)
+				_putchar('#');
+			if (hash == size)
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
