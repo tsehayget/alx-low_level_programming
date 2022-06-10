21ae138a3eb9e41dda741d4182d802d3b574856c
tsehayget <tsehayget4@gmail.com>
2022-06-09 19:17:11 -0700

    final commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,37 +6,15 @@
  */
 void print_number(int n)
 {
-	long m;
-	int c;
-	long num;
+	unsigned int num = n;
 
-	num = n;
-
-	if (num < 0)
+	if (n < 0)
 	{
-		num *= -1;
 		_putchar('-');
+		num = -num;
 	}
-	m = 1;
-	c = 1;
-	while (c)
-	{
-		if (num / (m * 10) > 0)
-			m *= 10;
-		else
-			c = 0;
-	}
-	while (num >= 0)
-	{
-		if (m == 1)
-		{
-			_putchar(num % 10 + '0');
-			num = -1;
-		}
-		else
-		{
-			_putchar((num / m % 10) + '0');
-			m /= 10;
-		}
-	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
+
 }
