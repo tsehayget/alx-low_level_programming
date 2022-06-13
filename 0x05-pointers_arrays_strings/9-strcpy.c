ab536206df2e5833336840de5941d3f4646e1efe
tsehayget <tsehayget4@gmail.com>
2022-06-13 13:52:31 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strcpy - copies string from dest to src
+ * @src: var
+ * @dest: var
+ * Return: returns stored value in dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
