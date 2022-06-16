0d10851fb26eba5cbe8e34f43ff9bb5f6203cc3a
tsehayget <tsehayget4@gmail.com>
2022-06-16 15:12:32 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
  * rot13 - replaces characters
+ *
  * @s: pointer string importing
- * Return: Returns final value in to pointer
+ * Return: Returns final value
  */
 char *rot13(char *s)
 {
-	char c1[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-	char c2[] = "nopqrstuvwxyzabcdefghijklm";
-	int i;
+	char l33t[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char real[52] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
-		if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
-		{
-			s[i] = (s[i] - 65 > 25) ?
-				c2[s[i] - 97] : c1[s[i] - 65];
-		}
-	}
+		for (j = 0; j < 52; j++)
+			if (s[i] == real[j])
+			{
+				s[i] = l33t[j];
+				break;
+			}
 	return (s);
 }
