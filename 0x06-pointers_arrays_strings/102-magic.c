cfa133f5deec7417a27ff0c3d1167b99fdf5e2d2
tsehayget <tsehayget4@gmail.com>
2022-06-16 16:03:14 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,14 +9,14 @@ int main(void)
 	int a[5];
 	int *p;
 
-	a[2] = 98;
+	a[2] = 1024;
 	p = &n;
 	/**
 	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
 	 */
-	*(p + 5);
+	*(p + 5) = 98;
 
 	printf("a[2] = %d\n", a[2]);
 	return (0);
