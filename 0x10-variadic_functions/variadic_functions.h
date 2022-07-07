2946ee87a91c4fdf34af7617160fff2bdef8191a
tsehayget <tsehayget4@gmail.com>
2022-07-07 06:55:07 -0700

    header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,13 +6,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-
-#include <stdarg.h>
-typedef struct print 
-{
-	char *s;
-	void (*print)(va_list arg);
-} print_t;
-
-
 #endif
